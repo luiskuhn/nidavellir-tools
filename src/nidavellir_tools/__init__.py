32f8bfee14657eb6fa85fde17f8127fde3948173
luiskuhn <luis.kuhn@gmail.com>
2026-09-14 16:07:58 +0200

    Prepare nidavellir-tools 0.3.0 release

--- src/nidavellir_tools/__init__.py
+++ src/nidavellir_tools/__init__.py
@@ -5,6 +5,6 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("nidavellir-tools")
 except PackageNotFoundError:  # Source checkout before installation.
-    __version__ = "0.2.0"
+    __version__ = "0.3.0"
 
 __all__ = ["__version__"]
